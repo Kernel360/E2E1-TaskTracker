6ecc519245741105fbf14a6bc5b63c870bec5e33
Chan <ieejo716@naver.com>
2023-10-30 19:53:38 +0900

    [FEAT] board 도메인 수정

--- proejct/src/main/java/org/fastcampus/proejct/board/dto/BoardDto.java
+++ proejct/src/main/java/org/fastcampus/proejct/board/dto/BoardDto.java
@@ -1,10 +1,12 @@
 package org.fastcampus.proejct.board.dto;
 
 import org.fastcampus.proejct.board.domain.Board;
+import org.fastcampus.proejct.board.domain.Task;
 import org.fastcampus.proejct.user.domain.UserInfo;
 import org.fastcampus.proejct.user.dto.UserInfoDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * DTO for {@link org.fastcampus.proejct.board.domain.Board}
@@ -14,23 +16,25 @@ public record BoardDto(
         String title,
         String content,
         UserInfoDto userInfo,
+        List<TaskDto> tasks,
         String createdBy,
         LocalDateTime createdAt,
         String modifiedBy,
         LocalDateTime modifiedAt
 ) {
-
     public static BoardDto of(
             Long id,
             String title,
             String content,
-            UserInfoDto userInfo
+            UserInfoDto userInfo,
+            List<TaskDto> tasks
     ) {
         return BoardDto.of(
                 id,
                 title,
                 content,
                 userInfo,
+                tasks,
                 null,
                 null,
                 null,
@@ -43,12 +47,13 @@ public record BoardDto(
             String title,
             String content,
             UserInfoDto userInfo,
+            List<TaskDto> tasks,
             String createdBy,
             LocalDateTime createdAt,
             String modifiedBy,
             LocalDateTime modifiedAt
     ) {
-        return new BoardDto(id, title, content, userInfo, createdBy, createdAt, modifiedBy, modifiedAt);
+        return new BoardDto(id, title, content, userInfo, tasks,createdBy, createdAt, modifiedBy, modifiedAt);
     }
 
     public static BoardDto from(Board entity) {
@@ -57,10 +62,20 @@ public record BoardDto(
                 entity.getTitle(),
                 entity.getContent(),
                 UserInfoDto.from(entity.getUserInfo()),
+                entity.getTasks().stream().map(TaskDto::from).toList(),
                 entity.getCreatedBy(),
                 entity.getCreatedAt(),
                 entity.getModifiedBy(),
                 entity.getModifiedAt()
+        );
+    }
+
+    public Board toEntity(UserInfo userInfo) {
+        return Board.of(
+                id,
+                title,
+                content,
+                userInfo
         );
     }
 
@@ -76,14 +91,5 @@ public record BoardDto(
                 ", modifiedBy='" + modifiedBy + '\'' +
                 ", modifiedAt=" + modifiedAt +
                 '}';
-    }
-
-    public Board toEntity(UserInfo userInfo) {
-        return Board.of(
-                id,
-                title,
-                content,
-                userInfo
-        );
     }
 }
