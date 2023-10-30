888d96efd6e32ff61af6546dd57036832bdbb35f
Chan <ieejo716@naver.com>
2023-10-30 19:56:08 +0900

    [CHORE] 파일 변경으로 인한 커밋

--- proejct/src/main/java/org/fastcampus/proejct/user/repository/UserInfoRepository.java
+++ proejct/src/main/java/org/fastcampus/proejct/user/repository/UserInfoRepository.java
@@ -3,5 +3,6 @@ package org.fastcampus.proejct.user.repository;
 import org.fastcampus.proejct.user.domain.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserInfo, Long> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+
 }
