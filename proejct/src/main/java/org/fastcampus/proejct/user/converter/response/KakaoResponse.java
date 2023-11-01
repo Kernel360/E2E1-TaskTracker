31a31f111c3d112e335d7c951ee33d2b9895fe88
Chan <ieejo716@naver.com>
2023-11-01 19:21:39 +0900

    [FEAT] kakao response
    카카오 송수신시 필요한 내용

---
+++ proejct/src/main/java/org/fastcampus/proejct/user/converter/response/KakaoResponse.java
@@ -0,0 +1,35 @@
+package org.fastcampus.proejct.user.converter.response;
+
+import org.fastcampus.proejct.user.converter.dto.KakaoAccountDto;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public record KakaoResponse(
+        Long id,
+        LocalDateTime connectedAt,
+        KakaoAccountDto kakaoAccountDto,
+        Map<String, Object> attributes
+) {
+    public static KakaoResponse from(Map<String, Object> attributes) {
+        return new KakaoResponse(
+                Long.valueOf(String.valueOf(attributes.get("id"))),
+                LocalDateTime.parse(
+                        String.valueOf(attributes.get("connected_at")),
+                        DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.systemDefault())
+                ),
+                KakaoAccountDto.from((Map<String, Object>) attributes.get("kakao_account")),
+                (Map<String, Object>) attributes.get("properties")
+        );
+    }
+
+    public String email() {
+        return this.kakaoAccountDto().email();
+    }
+
+    public String nickname() {
+        return this.kakaoAccountDto().nickname();
+    }
+}
