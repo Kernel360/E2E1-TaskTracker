d8678048e13cf32b0f40eeb3c79f1c5a7c60afa1
Chan <ieejo716@naver.com>
2023-11-03 18:28:27 +0900

    [FEAT] security 임시 제외
    개발 및 테스트 환경에 있어 불편함이 있기 때문에 잠시 주석처리

--- proejct/src/main/java/org/fastcampus/proejct/home/controller/MainController.java
+++ proejct/src/main/java/org/fastcampus/proejct/home/controller/MainController.java
@@ -1,19 +1,16 @@
 package org.fastcampus.proejct.home.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.fastcampus.proejct.global.converter.RedirectType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 public class MainController {
     @GetMapping("/")
     public String root() {
-        return Redirect.getInstance();
+        return RedirectType.REDIRECT_USER.getUrl();
+//        return Redirect.getInstance(); todo 개발 환경이 끝나면 다시 사용
     }
 }
