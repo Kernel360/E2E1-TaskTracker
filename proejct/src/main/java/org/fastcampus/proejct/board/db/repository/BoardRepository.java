05a73accac62bb1ca30cfc7e6b3e5a33ee0613f2
Chan <ieejo716@naver.com>
2023-11-06 16:11:45 +0900

    [FEAT] 게시판 sort 기능 추가
    SortType enum 클래스에  명세

--- proejct/src/main/java/org/fastcampus/proejct/board/db/repository/BoardRepository.java
+++ proejct/src/main/java/org/fastcampus/proejct/board/db/repository/BoardRepository.java
@@ -10,4 +10,20 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query("SELECT b FROM Board b WHERE b.title LIKE :keyword")
     List<Board> findByKeyword(@Param("keyword") String keyword);
+
+//    @Query("SELECT DISTINCT b FROM Board b LEFT JOIN b.members m\n" +
+//            "WHERE (b.userInfo.id = :userId OR m.userInfo.id = :userId) AND b.isFinished = false")
+//    List<Board> searchBoardByDefault(Long userId); //SORT_DEFAULT
+
+//    @Query("SELECT DISTINCT b FROM Board b LEFT JOIN b.members m\n" +
+//            "WHERE (b.userInfo.id = :userId OR m.userInfo.id = :userId) AND b.isFinished = true")
+//    List<Board> searchBoardByFinished(Long userId); //SORT_FINISHED
+//
+//    @Query("SELECT DISTINCT b FROM Board b LEFT JOIN b.members m\n" +
+//            "WHERE b.userInfo.id = :userId OR m.userInfo.id = :userId")
+//    List<Board> searchBoardByFinished(Long userId); //SORT_ALL
+
+    @Query("SELECT b FROM Board b WHERE b.userInfo.id = :userId")
+    List<Board> searchBoardSelf(Long userId); // SORT_SELF
+
 }
