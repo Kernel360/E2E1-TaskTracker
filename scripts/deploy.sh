dbef830b8077e68c58acbebbeecd8fcc43a2b2ae
Chan <72330632+oxix97@users.noreply.github.com>
2023-11-14 19:48:47 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> 현재 시간: $(date)" >> /home/ec2-user/action/deploy.log
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
 echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
+DEPLOY_PATH=/home/ec2-user/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
