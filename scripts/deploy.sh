dc946ab458b799e3b35a328761ed63914fcb7946
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-11-15 16:20:06 +0900

    AWS_TEST01

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/action/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
+#!/usr/bin/env bash
 
-echo "> 현재 시간: $(date)" >> /home/ec2-user/action/deploy.log
+REPOSITORY=/home/ec2-user/E2E1-TaskTracker
+cd $REPOSITORY
 
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
+APP_NAME=E2E1-TaskTracker
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
+  echo "> 종료할것 없음."
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ec2-user/action/deploy.log
-  sudo kill -9 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
