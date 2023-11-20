bee99912c4483d76cd44c86f3300b5dfbe22a6ba
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-11-20 16:57:07 +0900

    followcontroller 추가

--- src/main/java/org/fastcampus/proejct/follow/controller/FollowController.java
+++ src/main/java/org/fastcampus/proejct/follow/controller/FollowController.java
@@ -2,39 +2,30 @@ package org.fastcampus.proejct.follow.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.fastcampus.proejct.auth.converter.dto.UserPrincipal;
 import org.fastcampus.proejct.follow.converter.FollowDto;
 import org.fastcampus.proejct.follow.service.FollowService;
-import org.springframework.web.bind.annotation.*;
+import org.fastcampus.proejct.user.converter.UserInfoDto;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/follow")
-@RestController
+@Controller
 public class FollowController {
     private final FollowService followService;
 
-    @GetMapping("/{followerId}/list")
-    public List<FollowDto> getFollows(
-            @PathVariable Long followerId
-    ) {
-        return followService.getFollow(followerId);
-    }
-
-    @PostMapping("{followerId}/send/{followingId}")
-    public void saveFollow(
-            @PathVariable Long followerId,
-            @PathVariable Long followingId
-    ) {
-        followService.saveFollow(followerId, followingId);
-    }
-
-    @DeleteMapping("{followerId}/delete/{followingId}")
-    public void deleteFollow(
-            @PathVariable Long followerId,
-            @PathVariable Long followingId
-    ) {
-        followService.deleteFollow(followerId, followingId);
+    @GetMapping("follow/list")
+    public String getFollowView(@AuthenticationPrincipal UserPrincipal userPrincipal, Model model) {
+        UserInfoDto principal = userPrincipal.toDto();
+        List<FollowDto> followList = followService.getFollow(principal.id());
+        model.addAttribute("follows", followList);
+        return "top";
     }
 }
