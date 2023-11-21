62a476ca7e9f8acdb7219e8cc9a289e8e59e2cfc
Chan <ieejo716@naver.com>
2023-11-21 19:33:03 +0900

    [FEAT] 게시판 뷰 연동
    - 게시판 친구 목록 수정
    - 게시판 할일 목록 수정

--- src/main/java/org/fastcampus/proejct/follow/db/repository/FollowRepository.java
+++ src/main/java/org/fastcampus/proejct/follow/db/repository/FollowRepository.java
@@ -2,6 +2,7 @@ package org.fastcampus.proejct.follow.db.repository;
 
 import org.fastcampus.proejct.follow.db.model.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/org/fastcampus/proejct/follow/service/FollowService.java
+++ src/main/java/org/fastcampus/proejct/follow/service/FollowService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.fastcampus.proejct.follow.converter.FollowDto;
 import org.fastcampus.proejct.follow.db.model.Follow;
 import org.fastcampus.proejct.follow.db.repository.FollowRepository;
+import org.fastcampus.proejct.user.converter.UserInfoDto;
+import org.fastcampus.proejct.user.db.repository.UserInfoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +18,9 @@ import java.util.List;
 @Transactional
 @Service
 public class FollowService {
-    private  final FollowRepository followRepository;
+    private final FollowRepository followRepository;
+    private final UserInfoRepository userInfoRepository;
+
     @Transactional(readOnly = true)
     public List<FollowDto> getFollow(Long follower) {
         return followRepository.findAllByFollower(follower).stream()
@@ -24,16 +28,25 @@ public class FollowService {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
+    public List<UserInfoDto> getFollowingUsers(Long follower) {
+        return followRepository.findAllByFollower(follower).stream()
+                .map(FollowDto::from)
+                .map(it -> userInfoRepository.getReferenceById(it.following()))
+                .map(UserInfoDto::from)
+                .toList();
+    }
+
     public void saveFollow(Long follower, Long following) {
-        Follow follow = Follow.of(follower,following);
-        Follow follow2 = Follow.of(following,follower);
+        Follow follow = Follow.of(follower, following);
+        Follow follow2 = Follow.of(following, follower);
         followRepository.save(follow);
         followRepository.save(follow2);
     }
 
     public void deleteFollow(Long follower, Long following) {
-        followRepository.deleteFollowByFollowerAndFollowing(follower,following);
-        followRepository.deleteFollowByFollowerAndFollowing(following,follower);
+        followRepository.deleteFollowByFollowerAndFollowing(follower, following);
+        followRepository.deleteFollowByFollowerAndFollowing(following, follower);
     }
 
 }
