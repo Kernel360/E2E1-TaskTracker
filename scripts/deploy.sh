4eb0b681fbce116a7c28b8e3db29b9c251fc4542
gunsight1 <gunsight777@gmail.com>
2023-11-23 13:52:50 +0900

    수정된 배포환경파일 업로드

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/log/deploy_success.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/log/deploy_success.log
 else
@@ -25,9 +25,6 @@ else
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
-
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/log/deploy_success.log
